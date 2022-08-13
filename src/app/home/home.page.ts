1efebea84fbf515fddcb50785261c9ce71180006
alifmustaqim <46521041+alifmustaqim@users.noreply.github.com>
2022-08-13 10:50:08 +0800

    Fetch data as currency selector & amount input

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataService } from "../services/data.service";
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  symbols: any;
+  curAmount: any = '1.00';
+  constructor(private dataServer: DataService) {}
+  ngOnInit() {
+    this.getSymbols();
+  }
 
-  constructor() {}
+  // INITIAL
+  async getSymbols() {
+    await this.dataServer.getSymbols().then((data: any) => {
+      this.symbols = [];
+      for (var key in data.symbols) {
+        if (data.symbols.hasOwnProperty(key)) {
+          this.symbols.push({ "code": data.symbols[key].code, "desc": data.symbols[key].description });
+        }
+      }
+    });
+  }
+  selectedCur(ev) {
+    let selCur = ev.detail.value;
+    let a = selCur.split("|");
+  }
 
 }
