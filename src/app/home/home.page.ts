6344df6b2e436f4d9578278ef6ef19126fd4eda6
alifmustaqim <46521041+alifmustaqim@users.noreply.github.com>
2022-08-13 11:19:19 +0800

    Convert and display data by selection

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from "../services/data.service";
+import { LoadingController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,8 +9,15 @@ import { DataService } from "../services/data.service";
 })
 export class HomePage {
   symbols: any;
+  codeCur: any = '';
+  descCur: any;
+  seleCur: any;
+  convData: any = [];
+  ratesData: any = [];
+  crcTerm: any;
+  filterCur: any;
   curAmount: any = '1.00';
-  constructor(private dataServer: DataService) {}
+  constructor(private dataServer: DataService,public loader: LoadingController) {}
   ngOnInit() {
     this.getSymbols();
   }
@@ -24,9 +33,36 @@ export class HomePage {
       }
     });
   }
+  async convertCurrency(base, amount) {
+    await this.dataServer.convCur(base, amount).then((data: any) => {
+      this.ratesData = Object.keys(data.rates)
+        .map(key => ({ name: key, value: data.rates[key] }))
+    });
+    this.convData = [];
+    for (let i = 0; i < this.symbols.length; i++) {
+      this.convData.push({
+        ...this.symbols[i],
+        ...(this.ratesData.find((itmInner) => itmInner.name === this.symbols[i].code))
+      }
+      );
+    }
+  }
+
+  async getRates() {
+    const loading = await this.loader.create({
+      message: 'Changing the currency',
+      duration: 2000
+    });
+    await loading.present();
+    await this.convertCurrency(this.codeCur, this.curAmount);
+  }
+
   selectedCur(ev) {
     let selCur = ev.detail.value;
     let a = selCur.split("|");
+    this.codeCur = a[0];
+    this.descCur = a[1];
+    this.seleCur = a[0] + ' - ' + a[1];
   }
 
 }
